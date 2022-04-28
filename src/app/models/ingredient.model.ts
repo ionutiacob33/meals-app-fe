a12e7ba9387ff8d613779ee03ac6eac25a5b054b
ionutiacob33 <ionut.iacobmare@gmail.com>
2022-04-28 20:41:24 +0300

    Added ingredient model and refactored models

---
+++ src/app/models/ingredient.model.ts
@@ -0,0 +1,8 @@
+export class Ingredient {
+  constructor(
+    public id: number,
+    public ingredient: string,
+    public unit: string,
+    public quantity: number,
+  ) { }
+}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {Recipe} from "./recipe.model";
+import {Recipe} from "../models/recipe.model";
 
 @Injectable()
 export class RecipeService {
